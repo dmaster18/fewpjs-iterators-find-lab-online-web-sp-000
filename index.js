56dc2e4423cbcc237630f3a28de6ba35c7d607fc
David Strauch <dmaster18@gmail.com>
2020-08-28 16:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const record = [
 
 function superbowlWin(record) {
   let game = record.find(r => r.result === "W");
-  if (typeof game !== undefined || game !== null) {
+  if (typeof game !== undefined) {
     return game.year;} else {
       return undefined;
     }
