0f8abcbf6fb6b91121a00a945f32162ea8fa9321
David Strauch <dmaster18@gmail.com>
2020-08-28 16:46:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  {year: "2015", result: "W"}
+]
+
+function superbowlWin(record) {
+  let game = record.find(r => r.result === "W");
+  if (typeof game !== undefined) {
+    return game.year;} else {
+      return undefined;
+    }
+}
